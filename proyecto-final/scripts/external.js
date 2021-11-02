11ff50b7f61ed0c3dc123abf5df1f3081e12d32d
Mercedes <mercechinchilla@gmail.com>
2021-11-01 22:51:18 -0600

    test

---
+++ proyecto-final/scripts/external.js
@@ -0,0 +1,8 @@
+function showFunction(diva, divb) {
+    var x = document.getElementById(diva);
+    var y = document.getElementById(divb);
+          x.style.display = 'block';
+          y.style.display = 'none';
+}
+
+
